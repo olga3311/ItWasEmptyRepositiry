3d587313f7dd2aa2a6d6c8cfb4addff1fae3dbb2
olga3311 <167119299+olga3311@users.noreply.github.com>
2024-07-09 17:00:56 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("It was edited from Git");
+        System.out.println(" Third Branch");         
     }
 }
